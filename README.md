3250dc3f9cc3f15b61830724973707b44e853ea8
dydtmd6801 <dydtmd6801@naver.com>
2021-11-20 03:44:11 -0800

    index_footer_maked

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # php_project
+# YNC 2학년 2학기 php
+# 기말 프로젝트
---
+++ index.php
@@ -0,0 +1,86 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/reset.css">
+    <link rel="stylesheet" href="./css/index.css">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;700&display=swap" rel="stylesheet">
+    <title>Document</title>
+</head>
+<body>
+    <header id="main_header">
+        <div id="top_gnb">
+            <div id="main_logo">Yong'S in Daegu</div>
+            <div id="main_menu">
+                <ul class="main_top_menu">
+                    <a href="#"><li class="top_sub_menu">대구 놀거리</li></a>
+                    <a href="#"><li class="top_sub_menu">대구 먹거리</li></a>
+                    <a href="#"><li class="top_sub_menu">사람들 이야기</li></a>
+                    <a href="#"><li onclick="show_login_menu()">img</li></a>
+                </ul>
+            </div>
+        </div>
+    </header>
+    <section id="main_section">
+        <div>
+        </div>
+    </section>
+    <footer id="main_footer">
+        <!-- <div id="maker_info">
+            <p>영남이공대학교 | 1805021 유용승</p>
+            <p>영남이공대학교 2학년 2학기 | php 기말 프로젝트</p>
+        </div>
+        <div id="maker_info">
+            <ul id="maker_site">
+                <a href="">
+                    <li class="site_menu">notion</li>
+                </a>
+                <a href="">
+                    <li class="site_menu">github</li>
+                </a>
+                <a href="">
+                    <li class="site_menu">facebook</li>
+                </a>
+                <a href="">
+                    <li class="site_menu">instagram</li>
+                </a>
+            </ul>   
+        </div> -->
+        <div id="footer_size">
+            <div id="footer_logo">
+                <p>Yong'S in Daegu</p>
+            </div>
+            <div id="footer_text">
+                <div id="maker">
+                    <div class="center">
+                        <p class="footer_title">만든사람</p>
+                        <p class="footer_content">영남이공대학교</p>
+                        <p class="footer_content">1805021 유용승</p>
+                    </div>
+                </div>
+                <div id="readme">
+                    <div class="center">
+                        <p class="footer_title">페이지 소개</p>
+                        <p class="footer_content">영남이공대학교 2학년 2학기</p>
+                        <p class="footer_content">PHP 기말 프로젝트</p>
+                        <p class="footer_content">대구 놀거리 먹거리 소개</p>
+                    </div>
+                </div>
+                <div id="maker_info">
+                    <div class="center">
+                        <p class="footer_title">개발자 이력</p>
+                        <p class="footer_content">notion</p>
+                        <p class="footer_content">github</p>
+                        <p class="footer_content">facebook</p>
+                        <p class="footer_content">instagram</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </footer>
+</body>
+</html>
