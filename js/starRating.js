52782e7a645d3a0dc5c8ed77b498c2a69a82fe43
dydtmd6801 <dydtmd6801@naver.com>
2021-12-01 21:47:45 -0800

    make_review_star

---
+++ js/starRating.js
@@ -0,0 +1,60 @@
+var star5 = document.getElementsByClassName('star_img')[0];
+var star4 = document.getElementsByClassName('star_img')[1];
+var star3 = document.getElementsByClassName('star_img')[2];
+var star2 = document.getElementsByClassName('star_img')[3];
+var star1 = document.getElementsByClassName('star_img')[4];
+
+function change_img(show_cnt, cla_value, img_src_fill, img_src_empty){
+    for(var i = 5 - show_cnt; i < 5; i++){
+        document.getElementsByClassName(cla_value)[i].src=img_src_fill;
+    }
+    for(var i = 0; i < 5 - show_cnt; i++){
+        document.getElementsByClassName(cla_value)[i].src=img_src_empty;
+    }
+}
+
+function check_star(check_url){
+    if(check_url == "http://192.168.244.128/php_project/img/star_empty.png"){
+        return false;
+    } else {
+        return true;
+    }
+}
+
+star5.addEventListener('click', function(){select_img(5)});
+star4.addEventListener('click', function(){select_img(4)});
+star3.addEventListener('click', function(){select_img(3)});
+star2.addEventListener('click', function(){select_img(2)});
+star1.addEventListener('click', function(){select_img(1)});
+function select_img(star_cnt){
+    var img_src_fill = "./img/star.png";
+    var img_src_empty = "./img/star_empty.png";
+    var cla_value = "star_img";
+    var check_star5 = check_star(star5.src);
+    var check_star4 = check_star(star4.src);
+    var check_star3 = check_star(star3.src);
+    var check_star2 = check_star(star2.src);
+    switch(star_cnt){
+        case 1:
+            console.log(star1.src);
+            if(check_star5 == false && check_star4 == false && check_star3 == false && check_star2 == false){
+                if(star1.src == "http://192.168.244.128/php_project/img/star.png"){
+                    img_src_fill = "./img/star_empty.png";
+                }
+            }
+            change_img(1, cla_value, img_src_fill, img_src_empty);
+            break;
+        case 2:
+            change_img(2, cla_value, img_src_fill, img_src_empty);
+            break;
+        case 3:
+            change_img(3, cla_value, img_src_fill, img_src_empty);
+            break;
+        case 4:
+            change_img(4, cla_value, img_src_fill, img_src_empty);
+            break;
+        case 5:
+            change_img(5, cla_value, img_src_fill, img_src_empty);
+            break;
+    }
+}
--- review_insert.php
+++ review_insert.php
@@ -7,18 +7,50 @@
     <link rel="stylesheet" href="./css/reset.css">
     <link rel="stylesheet" href="./css/header.css">
     <link rel="stylesheet" href="./css/footer.css">
+    <link rel="stylesheet" href="./css/starRating.css">
     <link rel="stylesheet" href="./css/no_scroll.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Noto+Sans+KR:wght@300;400;700&display=swap" rel="stylesheet">
+    <script async src="./js/starRating.js"></script>
     <title>Document</title>
+    <style>
+        .star_img{
+            width: 30px;
+            height: 30px;
+        }
+        .star_img:hover{
+            filter: drop-shadow(1px 1px 4px gold);
+        }
+    </style>
 </head>
 <body>
     <?php include "header.php"; ?>
     <section>
-        <?php
-        echo $_GET["subject"];
-        ?>
+        <form action="review_insert.php" method="post">
+            <div id="review_form">
+                <div id="review_subject">
+                    제목 : <?php echo $_GET["subject"]; ?>
+                </div>
+                <div id="review_star">                    
+                    <div class="star-rating">
+                        <input type="radio" id="5-stars" name="rating" value="5" />
+                        <label for="5-stars" class="star"><img src="./img/star_empty.png" class="star_img"/></label>
+                        <input type="radio" id="4-stars" name="rating" value="4" />
+                        <label for="4-stars" class="star"><img src="./img/star_empty.png" class="star_img"/></label>
+                        <input type="radio" id="3-stars" name="rating" value="3" />
+                        <label for="3-stars" class="star"><img src="./img/star_empty.png" class="star_img"/></label>
+                        <input type="radio" id="2-stars" name="rating" value="2" />
+                        <label for="2-stars" class="star"><img src="./img/star_empty.png" class="star_img"/></label>
+                        <input type="radio" id="1-star" name="rating" value="1" />
+                        <label for="1-star" class="star"><img src="./img/star_empty.png" class="star_img"/></label>
+                    </div>
+                </div>
+                <div id="review_content">
+                    <textarea></textarea>
+                </div>
+            </div>
+        </form>
     </section>
     <?php include "footer.html"; ?>
 </body>
