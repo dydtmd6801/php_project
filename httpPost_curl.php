b9b3e6959f259c9f657b02a87b83204303724c20
dydtmd6801 <dydtmd6801@naver.com>
2023-03-19 21:45:35 -0700

    redirect로 인해 curl 옵션 추가

--- httpPost_curl.php
+++ httpPost_curl.php
@@ -1,18 +1,24 @@
 <?php
 function httpPost($url) {
-    $postData = '';
+    $res = array();
+    $options = array( 
+        CURLOPT_RETURNTRANSFER => true,     // return web page 
+        CURLOPT_HEADER         => false,    // do not return headers 
+        CURLOPT_FOLLOWLOCATION => true,     // follow redirects 
+        CURLOPT_USERAGENT      => "spider", // who am i 
+        CURLOPT_AUTOREFERER    => true,     // set referer on redirect 
+        CURLOPT_CONNECTTIMEOUT => 120,      // timeout on connect 
+        CURLOPT_TIMEOUT        => 120,      // timeout on response 
+        CURLOPT_MAXREDIRS      => 10,       // stop after 10 redirects 
+    ); 
 
-    $ch = curl_init();
-
-    curl_setopt($ch,CURLOPT_URL,$url);
-    curl_setopt($ch,CURLOPT_RETURNTRANSFER,true);
-    curl_setopt($ch,CURLOPT_HEADER, false); 
-    curl_setopt($ch, CURLOPT_POST, count($postData));
-    curl_setopt($ch, CURLOPT_POSTFIELDS, $postData);    
-
-    $output = curl_exec($ch);
-
-    curl_close($ch);
-    return $output;        
+    $ch      = curl_init( $url ); 
+    curl_setopt_array( $ch, $options ); 
+    $content = curl_exec( $ch ); 
+    $err     = curl_errno( $ch ); 
+    $errmsg  = curl_error( $ch ); 
+    $header  = curl_getinfo( $ch ); 
+    curl_close( $ch ); 
+    return $content;
 }
 ?>
